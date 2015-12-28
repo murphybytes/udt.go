366b29c51fba79c1ce38b1cd4b2abf558b2570f7
John Murphy <murphybytes@gmail.com>
2015-12-28 13:25:00 +0800

    corrected paths and compiler

--- udt/udt.go
+++ udt/udt.go
@@ -1,6 +1,7 @@
 package udt
 
-// #cgo LDFLAGS: -L${SRCDIR}/../vendor/git.code/sf.net/udt/udt4/src -ludt
-// #cgo CFLAGS: -I${SRCDIR}/../vendor/git.code/sf.net/udt/udt4/src
+// #cgo CXX: g++
+// #cgo LDFLAGS: -L${SRCDIR}/../vendor/udt/udt4/src -ludt
+// #cgo CFLAGS: -I${SRCDIR}/../vendor/udt/udt4/src
 // #include <udt.h>
 import "C"
